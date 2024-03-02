16d50d2adbeb7048c6d66e0b354adb6af39a3546
julioorozco05 <julio11orozco09@gmail.com>
2024-03-01 21:13:18 -0600

    contador de numeros

--- main.py
+++ main.py
@@ -2,5 +2,13 @@ def saludo():
     nombre_usuario = input("¿cuál es su nombre?: ")
     print(f"saludos {nombre_usuario}, ¿cómo está?")
 
+def contador_numero():
+    numero = int(input("escoga el numero: "))
+    contador = 0
+    while contador < numero:
+        contador += 1
+        print(contador)
+
 if __name__ == "__main__":
     saludo()
+    contador_numero()
